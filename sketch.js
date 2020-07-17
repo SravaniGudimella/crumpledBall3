f3f8d31a3f0984c95fcf11305a397b3931e452b9
SravaniGudimella <65073264+SravaniGudimella@users.noreply.github.com>
2020-07-17 07:56:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var Dustbin1, dustbin2, dustbin3;
+var paper,ground;
+var slingShot;
+var box1,box2,box3;
+
+function preload()
+{
+
+}
+
+function setup() {
+	
+	createCanvas(800, 700);
+
+	
+	engine = Engine.create();
+	world = engine.world;
+	
+	paper = new Paper(100, 350,45);
+	
+	ground = new Ground(400,580,800,20);
+	
+	box1 = new Box(700, 500, 150,150);	
+	
+
+	slingShot = new Launcher(paper.body,{x:100,y:350});
+	
+	
+	Engine.run(engine);
+	
+}
+
+
+function draw(){
+  background("cyan");
+ 
+  Engine.update(engine);
+ 
+  
+  paper.display();
+  ground.display();
+  slingShot.display();
+  box1.display();
+  
+}
+/*function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:360,y:-209});
+	   
+	 }
+}*/
+function mouseDragged(){
+    Matter.Body.setPosition(paper.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingShot.fly();
+}
